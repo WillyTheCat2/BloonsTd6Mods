c05b90657b9b6676e8c34447fbc42424ea7a81e8
WillyTheCat2 <86013105+WillyTheCat2@users.noreply.github.com>
2022-08-26 20:43:17 -0500

    made mod

--- PokemonTowers/ModHelperData.cs
+++ PokemonTowers/ModHelperData.cs
@@ -7,6 +7,6 @@ public static class ModHelperData
 
     public const string Description = "An empty mod";
 
-    public const string RepoOwner = ""; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = ""; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoOwner = "WIllyTheCat2"; // TODO add your github username hero, also in the download url in README.md
+    public const string RepoName = "BloonsTd6Mods"; // TODO add your repo name here, also in the download url in README.md
 }
